f364fea63c3a94ce2628341247f605a71874adcb
laurynwatkins <lwatkins@usf.edu>
2025-01-18 12:35:27 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+server.get('/hello', function (req, res) {
+res.send('Hello World! My name is Lauryn.');
+});
+server.listen(port, function () {
+console.log('Listening on ' + port);
+});
