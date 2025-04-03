2e66208245ea9c16f80a44939674adc17284b6b5
laurynwatkins <lwatkins@usf.edu>
2025-04-03 13:10:47 -0400

    Add files via upload

---
+++ api/names/page.tsx
@@ -0,0 +1,24 @@
+import { JSX } from "react";
+
+export default async function ServerComponentUserList (): Promise<JSX.Element> {
+        const url = "https://www.usemodernfullstack.dev/api/v1/users";
+        let data: ResponseType[] | [] = [];
+        let names: ResponseType[] | []; 
+
+        try {
+            const response = await fetch(url, { cache: "force-cache" })
+            data = (await response.json()) as ResponseType[];
+        } catch (err) {
+            throw new Error("Failed to fetch data");
+        }
+        names = data.map((item) => {
+            return { id: item.id, name: item.name };
+        });
+      return (
+        <ul>
+            {names.map((item) => (
+                <li key={item.id}>{item.id}.{item.name}</li>
+            ))}
+        </ul>
+    );  
+}
